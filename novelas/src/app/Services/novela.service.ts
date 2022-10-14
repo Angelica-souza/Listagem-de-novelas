ecd7ac7a93db2a402f37f4b2fa061d1f8330b328
Angelica-souza <angelicatecinfor@gmail.com>
2022-10-13 22:20:16 -0300

    Adcionando serviço

--- novelas/src/app/Services/novela.service.ts
+++ novelas/src/app/Services/novela.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { API_PATH } from 'src/environments/environment';
 import { INovela } from '../Interface/iNovelas';
+import { NovelaModel } from '../Interface/novela.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class NovelaService {
 
   listarNovela(): Promise<INovela[]>{
     return lastValueFrom(this.httpClient.get<INovela[]>(`${API_PATH}/novelas`));
+  }
+
+  async registerNovela(novela: NovelaModel) : Promise<void> {
+    await lastValueFrom(this.httpClient.post<NovelaModel[]>(`${API_PATH}/novelas`, novela))
   }
 }
